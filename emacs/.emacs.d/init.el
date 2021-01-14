0f8bd8fa9650aef330147c21023e23fad379644e
ishbosamiya <ishbosamiya@gmail.com>
2021-01-14 11:38:43 +0530

    emacs: rust: initial configuration

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -21,7 +21,7 @@
  '(inhibit-startup-screen t)
  '(package-selected-packages
    (quote
-    (arduino-mode scad-preview scad-mode pdf-tools ag glsl-mode smex elpy ess ac-clang clang-format ggtags writegood-mode org auto-complete-c-headers company company-c-headers auto-complete))))
+    (flycheck-popup-tip flycheck-pos-tip flycheck-rust racer cargo rust-mode arduino-mode scad-preview scad-mode pdf-tools ag glsl-mode smex elpy ess ac-clang clang-format ggtags writegood-mode org auto-complete-c-headers company company-c-headers auto-complete))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -233,3 +233,52 @@
 
 ;; Org mode settings
 (setq org-src-fontify-natively t)
+
+;; Rust configuration
+(use-package rust-mode
+  :ensure t
+  :config
+  (setq rust-format-on-save t
+        rust-format-show-buffer nil)
+  (add-hook 'rust-mode-hook
+            (lambda ()
+              (setq indent-tabs-mode nil)
+              ;; Prevent rust from hijacking the nice fold-this mode
+              (define-key rust-mode-map (kbd "C-c C-f") nil))))
+(use-package cargo
+  :ensure t
+  :after rust-mode
+  :hook (rust-mode . cargo-minor-mode))
+(use-package racer
+  :ensure t
+  :after rust-mode
+  :hook (rust-mode . racer-mode)
+  :config
+  (add-hook 'racer-mode-hook #'eldoc-mode)
+  (add-hook 'racer-mode-hook #'company-mode)
+  ;; (define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
+  (setq company-tooltip-align-annotations t)
+  (setq racer-rust-src-path
+        ;; Workaround for changes to where std is stored until (see
+        ;; https://github.com/racer-rust/emacs-racer/pull/143)
+      (let* ((sysroot (string-trim
+                       (shell-command-to-string "rustc --print sysroot")))
+             (lib-path (concat sysroot "/lib/rustlib/src/rust/library"))
+              (src-path (concat sysroot "/lib/rustlib/src/rust/src")))
+        (or (when (file-exists-p lib-path) lib-path)
+            (when (file-exists-p src-path) src-path))))
+  :bind (:map racer-mode-map
+         ("C-'" . racer-find-definition-other-window)))
+(use-package flycheck-rust
+  :ensure t
+  :after rust-mode
+  :hook (flycheck-mode . flycheck-rust-setup)
+  :hook (rust-mode . flycheck-mode))
+(use-package flycheck-pos-tip
+  :ensure t
+  ;; :hook (rust-mode . flycheck-pos-tip-mode)
+)
+(use-package flycheck-popup-tip
+  :ensure t
+  :hook (rust-mode . flycheck-popup-tip-mode)
+)
