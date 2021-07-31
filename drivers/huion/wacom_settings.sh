21f348342e4d096afa8a73b8f00bd9bf0f0f62e3
ishbosamiya <ishbosamiya@gmail.com>
2021-07-31 13:20:28 +0530

    drivers: huion: driver and configuration files
    digiment dkms is really powerful.
    the 52-tablet.conf file should go within /etc/X11/xorg.conf.d/
    
    see https://github.com/DIGImend/digimend-kernel-drivers for more details.

---
+++ drivers/huion/wacom_settings.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+xsetwacom set "Tablet Monitor Pen stylus" Button 2 3
+xsetwacom set "Tablet Monitor Pen stylus" Button 3 2
+xsetwacom set "Tablet Monitor Pen stylus" MapToOutput HEAD-2
+xsetwacom set "Tablet Monitor Pad pad" button 9 "key +ctrl z -ctrl"
+xsetwacom set "Tablet Monitor Pad pad" button 1 "key b"
+xsetwacom set "Tablet Monitor Pad pad" button 2 "key l"
+xsetwacom set "Tablet Monitor Pad pad" button 3 "key e"
+xsetwacom set "Tablet Monitor Pad pad" button 8 "key +ctrl +shift z -shift -ctrl"
+xsetwacom --set "Tablet Monitor Pad pad" button 13 "key +space"
+xsetwacom --set "Tablet Monitor Pad pad" button 14 "key +ctrl"
