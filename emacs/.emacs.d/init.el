7448ad941574ddca960a63511429f30924671a54
ishbosamiya <ishbosamiya@gmail.com>
2022-03-09 17:48:16 +0530

    emacs: switch to compilation mode when comint process exits
    TODO: need to figure out combined sentinel stuff

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -545,3 +545,56 @@
 (use-package string-inflection
   :ensure t
   :bind ("C-c i" . string-inflection-cycle))
+
+(defun compilation-mode-on-exit-sentinel (process output)
+  "Process sentinel to set \"*compilation*\" buffer to
+compilation-mode once PROCESS dies."
+  (unless (process-live-p process)
+    (message "comint exited")
+    (let* ((buffer (process-buffer process)))
+      (when (string= (buffer-name buffer) "*compilation*")
+	  (with-current-buffer buffer
+	    (compilation-mode))))))
+
+(defun add-compilation-mode-on-exit-sentinel ()
+  "Replace current process sentinel with a new sentinel composed
+of the current one and
+`compilation-mode-on-exit-sentinel'."
+  (let* ((buffer (get-buffer "*compilation*"))
+	 (process (get-buffer-process buffer)))
+    (when (bound-and-true-p process)
+      (let* ((og-sentinel (process-sentinel process))
+	     (sentinel-list (-remove #'null
+				     (list og-sentinel #'compilation-mode-on-exit-sentinel))))
+
+	;; TODO: Need to figure out how to make a combined sentinel
+	;; work
+	;;
+	;; (combined-sentinel
+	;;  (lambda (process line)
+	;;    (--each sentinel-list
+	;;      (funcall it process line))))
+	;; (setf (process-sentinel process) combined-sentinel)
+
+	(if (> (length sentinel-list) 1)
+	    (message "TODO: need to combine the sentinels (total %d) , currently only applies the `compilation-mode-on-exit-sentinel`" (length sentinel-list)))
+	(--each sentinel-list (setf (process-sentinel process) it))))))
+
+(defvar compilation-mode-on-exit-comint-hook-has-run nil
+  "Whether or not `compilation-mode-on-exit-comint-hook' has run
+   or not. We need this buffer-local var to prevent the hook from
+   running several times, as can happen for example when calling
+   `shell'.")
+
+(defun async-funcall (function &optional buffer args delay)
+  "Run FUNCTION with ARGS in the buffer after a short DELAY."
+  (run-at-time (or delay 0.2) nil
+               `(lambda ()
+                  (with-current-buffer ,buffer ,(cons function args)))))
+
+(defun compilation-mode-on-exit-comint-hook ()
+  (unless compilation-mode-on-exit-comint-hook-has-run
+    (setq-local compilation-mode-on-exit-comint-hook-has-run t)
+    (async-funcall #'add-compilation-mode-on-exit-sentinel (current-buffer))))
+
+(add-hook 'comint-mode-hook #'compilation-mode-on-exit-comint-hook)
