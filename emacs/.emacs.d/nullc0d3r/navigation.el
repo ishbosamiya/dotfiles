57632cd19a97e2c952ff82c66c82116e4bf42b68
ishbosamiya <ishbosamiya@gmail.com>
2022-03-21 11:36:45 +0530

    emacs: navigation: abort if mouse leaves or kbd macro ends
    Still need to make it for when the user leaves the minibuffer.
    
    One possible solution would be to figure out which buffers have the
    goto-line-relative active and store the various variables required in
    lists in the order of the recursive edit. This would the user to use
    `enable-recursive-minibuffers` but that's a good thing right??

--- emacs/.emacs.d/nullc0d3r/navigation.el
+++ emacs/.emacs.d/nullc0d3r/navigation.el
@@ -17,7 +17,7 @@
 
 ;;; Code:
 
-(defvar goto-line-relative-for-buffer nil
+(defvar goto-line-relative--current-buffer nil
   "stores buffer for which `goto-line-relative` is active (if
   active)")
 
@@ -30,18 +30,23 @@
   was called")
 
 (defun goto-line-relative--cleanup ()
+  ;; Remove hooks that would help early exit `goto-line-relative`.
+  (remove-hook 'mouse-leave-buffer-hook 'navigation-abort)
+  (remove-hook 'kbd-macro-termination-hook 'navigation-abort)
+
+  ;; Remove the keymap override
+  (setq overriding-terminal-local-map nil)
+
   ;; It is not necessary the cleanup is called from the buffer that
   ;; created the `goto-line-relative` context, thus must use that
   ;; buffer.
-  (with-current-buffer goto-line-relative-for-buffer
+  (with-current-buffer goto-line-relative--current-buffer
     ;; change back to initial config for display-line-numbers-mode
     (display-line-numbers-set-type initial-dln-type)
     ;; turn off display-line-numbers-mode if it was not initially
     ;; active
     (funcall 'display-line-numbers-mode dln-mode-is-initially-active))
-  ;; Remove the keymap override
-  (setq overriding-terminal-local-map nil)
-  (setq goto-line-relative-for-buffer nil))
+  (setq goto-line-relative--current-buffer nil))
 
 ;; Goto line relative to the current line
 (defun goto-line-relative (&optional number-of-lines)
@@ -54,26 +59,38 @@ minibuffer if `number-of-lines` is not provided.
   (interactive)
   ;; TODO: make sure the context for `goto-line-relative` is removed
   ;; when the window changes (move out of minibuffer). See how
-  ;; `isearch-mode` handles it.
+  ;; `isearch-mode` handles it. When mouse is used to leave the buffer
+  ;; works (see mouse-leave-buffer-hook) already.  One solution to
+  ;; this could be to figure out which buffers have the
+  ;; `goto-line-relative` active and store the various variables
+  ;; required in lists in the order of the recursive edit. This would
+  ;; the user to use `enable-recursive-minibuffers` but that's a good
+  ;; thing right??
+
 
   ;; Make `navigation-mode-local-map` override all other maps. This is
   ;; done so that even something `C-g` can be overriden.
   (setq overriding-terminal-local-map navigation-mode-local-map)
 
-  (setq goto-line-relative-for-buffer (current-buffer))
-  (let* ((dln-mode-is-initially-active-temp (if display-line-numbers-mode
-								 t -1))
-	 (initial-dln-type-temp display-line-numbers-type))
-    (setq dln-mode-is-initially-active dln-mode-is-initially-active-temp)
-    (setq initial-dln-type initial-dln-type-temp)
-    ;; display line numbers relatively
-    (display-line-numbers-relative)
-    ;; read number-of-lines unless already provided
-    (unless number-of-lines
-      (setq number-of-lines (read-number "Lines to skip (+/-): ")))
-    (let* ((current-line-number (line-number-at-pos))
-	   (jump-to-line (+ current-line-number number-of-lines)))
-      (goto-line jump-to-line)))
+  (setq goto-line-relative--current-buffer (current-buffer))
+  (setq dln-mode-is-initially-active (if display-line-numbers-mode
+					 t -1))
+  (setq initial-dln-type display-line-numbers-type)
+  ;; display line numbers relatively
+  (display-line-numbers-relative)
+
+  ;; Add hooks to exit `goto-line-relative` early if needed. Is set
+  ;; after the necessary data for cleanup.
+  (add-hook 'mouse-leave-buffer-hook 'navigation-abort)
+  (add-hook 'kbd-macro-termination-hook 'navigation-abort)
+
+  ;; read number-of-lines unless already provided
+  (unless number-of-lines
+    (setq number-of-lines (read-number "Lines to skip (+/-): ")))
+  (let* ((current-line-number (line-number-at-pos))
+	 (jump-to-line (+ current-line-number number-of-lines)))
+    (goto-line jump-to-line))
+
   (goto-line-relative--cleanup))
 
 (defgroup navigation nil
@@ -119,7 +136,7 @@ mean more care must be taken since even `C-g` can be overridden.
 
 (defun navigation-abort ()
   (interactive)
-  (when goto-line-relative-for-buffer
+  (when goto-line-relative--current-buffer
     (goto-line-relative--cleanup))
   (minibuffer-keyboard-quit))
 
