a57a118bfbf179cfa201bb394db6876b95a25d0b
ishbosamiya <ishbosamiya@gmail.com>
2022-03-29 12:59:25 +0530

    emacs: custom_variables: update with new stuff

--- emacs/.emacs.d/custom_variables.el
+++ emacs/.emacs.d/custom_variables.el
@@ -3,14 +3,19 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auth-source-save-behavior nil)
  '(custom-enabled-themes '(tango-dark))
  '(flyspell-default-dictionary "english")
  '(global-auto-revert-mode t)
  '(inhibit-startup-screen t)
  '(package-selected-packages
-   '(string-inflection impatient-mode simple-httpd rg flycheck yasnippet cmake-mode restart-emacs lsp-python-ms magit general dap-mode fold-this dash lsp-ui lsp-mode iedit sourcetrail projectile ido-completing-read+ flx-ido amx which-key clang-format+ olivetti unfill centered-window cargo rust-mode arduino-mode scad-preview scad-mode pdf-tools ag glsl-mode smex ess ggtags writegood-mode org company company-c-headers))
+   '(command-log-mode fzf esup hl-todo hl-block-mode expand-region string-inflection impatient-mode simple-httpd rg flycheck yasnippet cmake-mode restart-emacs lsp-python-ms magit general dap-mode fold-this dash lsp-ui lsp-mode iedit sourcetrail projectile ido-completing-read+ flx-ido amx which-key clang-format+ olivetti unfill centered-window cargo rust-mode arduino-mode scad-preview scad-mode pdf-tools ag glsl-mode smex ess ggtags writegood-mode org company company-c-headers))
  '(safe-local-variable-values
-   '((projectile-project-run-cmd . "cd ../../rusted-keys/.dep/egui_glfw/ && cargo run --example simple_gui")
+   '((projectile-project-run-cmd . "cargo expand")
+     (projectile-project-test-cmd . "cd builder && cargo test")
+     (projectile-project-run-cmd . "cargo run")
+     (projectile-project-compilation-cmd . "cargo build")
+     (projectile-project-run-cmd . "cd ../../rusted-keys/.dep/egui_glfw/ && cargo run --example simple_gui")
      (projectile-project-run-cmd . "mold -run cargo run")
      (projectile-project-compilation-cmd . "mold -run cargo build")
      (eval progn
