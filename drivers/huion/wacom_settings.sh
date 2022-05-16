5e2a76b91359f294c089bb8d4e9c4712f9d67ee7
ishbosamiya <ishbosamiya@gmail.com>
2022-05-16 16:07:57 +0530

    drivers: huion: wacom_settings: update for new installation

--- drivers/huion/wacom_settings.sh
+++ drivers/huion/wacom_settings.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-xsetwacom set "Tablet Monitor Pen stylus" Button 2 3
-xsetwacom set "Tablet Monitor Pen stylus" Button 3 2
-xsetwacom set "Tablet Monitor Pen stylus" MapToOutput HEAD-2
+xsetwacom set "Tablet Monitor stylus" Button 2 3
+xsetwacom set "Tablet Monitor stylus" Button 3 2
+xsetwacom set "Tablet Monitor stylus" MapToOutput HEAD-2
 xsetwacom set "Tablet Monitor Pad pad" button 9 "key +ctrl z -ctrl"
 xsetwacom set "Tablet Monitor Pad pad" button 1 "key b"
 xsetwacom set "Tablet Monitor Pad pad" button 2 "key l"
