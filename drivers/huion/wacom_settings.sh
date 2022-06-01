a9a6d58989db9b956a05d6f9f8bc0bd0b50fac85
ishbosamiya <ishbosamiya@gmail.com>
2022-06-01 12:07:13 +0530

    drivers: huion: wacom_settings: map the stylus based on x11 geometry
    This ensures that as long as the monitors are in the same position, it
    will always work.
    
    Specifying using `HEAD-2` or similar can lead selecting the wrong
    monitor since this can vary based on which monitor was connected
    first. Specifying using the x11 geometry, it is guaranteed to be the
    correct monitor as long as the physical layout of the monitors does
    not change.
    
    The best way to handle this would be to create a GUI application that
    will generate these commands on the fly. Will be an interesting
    challenge.

--- drivers/huion/wacom_settings.sh
+++ drivers/huion/wacom_settings.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 xsetwacom set "Tablet Monitor stylus" Button 2 3
 xsetwacom set "Tablet Monitor stylus" Button 3 2
-xsetwacom set "Tablet Monitor stylus" MapToOutput HEAD-2
+# If the displays are
+#
+# 1 2
+#   3
+#
+# Map to 3.
+xsetwacom --set "Tablet Monitor stylus" MapToOutput "1920x1080+1920+1080"
 xsetwacom set "Tablet Monitor Pad pad" button 9 "key +ctrl z -ctrl"
 xsetwacom set "Tablet Monitor Pad pad" button 1 "key b"
 xsetwacom set "Tablet Monitor Pad pad" button 2 "key l"
