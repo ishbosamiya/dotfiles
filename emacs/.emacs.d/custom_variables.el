e1437262b454a336ba800e35afe3fbe0a3e49cb6
ishbosamiya <ishbosamiya@gmail.com>
2022-06-04 12:00:17 +0530

    emacs: custom_variables: more safe local variable values

--- emacs/.emacs.d/custom_variables.el
+++ emacs/.emacs.d/custom_variables.el
@@ -11,7 +11,10 @@
  '(package-selected-packages
    '(yaml-mode nix-repl nix-mode command-log-mode fzf esup hl-todo hl-block-mode expand-region string-inflection impatient-mode simple-httpd rg flycheck yasnippet cmake-mode restart-emacs lsp-python-ms magit general dap-mode fold-this dash lsp-ui lsp-mode iedit sourcetrail projectile ido-completing-read+ flx-ido amx which-key clang-format+ olivetti unfill centered-window cargo rust-mode arduino-mode scad-preview scad-mode pdf-tools ag glsl-mode smex ess ggtags writegood-mode org company company-c-headers))
  '(safe-local-variable-values
-   '((projectile-project-run-cmd . "cargo expand")
+   '((projectile-project-test-cmd . "cd gpu/gpu_switcheroo && cargo test")
+     (projectile-project-run-cmd . "cd gpu/gpu_switcheroo && cargo run")
+     (projectile-project-compilation-cmd . "cd gpu/gpu_switcheroo && cargo build")
+     (projectile-project-run-cmd . "cargo expand")
      (projectile-project-test-cmd . "cd builder && cargo test")
      (projectile-project-run-cmd . "cargo run")
      (projectile-project-compilation-cmd . "cargo build")
