7304786a96c3e9e82266089ad4a048be190e5131
ishbosamiya <ishbosamiya@gmail.com>
2022-06-04 12:00:01 +0530

    gpu: gpu_switcheroo: initial hello world

---
+++ gpu/gpu_switcheroo/.dir-locals.el
@@ -0,0 +1,3 @@
+((nil . ((projectile-project-compilation-cmd . "cd gpu/gpu_switcheroo && cargo build")
+	 (projectile-project-run-cmd . "cd gpu/gpu_switcheroo && cargo run")
+	 (projectile-project-test-cmd . "cd gpu/gpu_switcheroo && cargo test"))))
