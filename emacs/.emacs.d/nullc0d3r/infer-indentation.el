9e44a55aecf7d02d7fec3e924d3a4ef785d4a140
ishbosamiya <ishbosamiya@gmail.com>
2025-02-10 14:53:44 +0530

    emacs: nullc0d3r: infer-indentation: refactor: shift infer-* functions

--- emacs/.emacs.d/nullc0d3r/infer-indentation.el
+++ emacs/.emacs.d/nullc0d3r/infer-indentation.el
@@ -18,6 +18,43 @@
   "Indentation utils."
   :group 'convenience)
 
+(defun infer-indents-tabs-mode ()
+  "Infer to set/unset `indent-tabs-mode`.
+
+# Note
+
+Currently, it infers based on how many lines start with ` ` vs
+`\t`. This may change in the future"
+  (interactive)
+  ;; set `indent-tabs-mode` based on how many lines start with spaces
+  ;; and tabs
+  (let ((space-count (how-many "^  " (point-min) (point-max)))
+        (tab-count (how-many "^\t" (point-min) (point-max))))
+    ;; if they are equal, default to spaces
+    (if (>= space-count tab-count) (setq indent-tabs-mode nil) (setq indent-tabs-mode t)))
+  (message "Setting `indent-tabs-mode` to `%s`" indent-tabs-mode))
+
+(defun infer-tab-width ()
+  "Infer and set `tab-width` (or similar like `c-basic-offset`
+based on active mode).
+
+# Note
+
+Offset is determined by `tab-width` and offsets/levels set by
+individual modes like `c-basic-offset`, `js-indent-level`,
+etc. This method attempts to set the correct one based on the
+mode that is active."
+  (interactive)
+  (message "TODO: need to implement `infer-tab-width`"))
+
+(defun infer-indentation-style ()
+  "Infer and set the indentation style.
+
+This is a collection of calls to required `infer-*` methods."
+  (interactive)
+  (infer-indents-tabs-mode)
+  (infer-tab-width))
+
 ;;;###autoload
 (define-minor-mode infer-indentation-mode
   "Infer indentation used in the file"
